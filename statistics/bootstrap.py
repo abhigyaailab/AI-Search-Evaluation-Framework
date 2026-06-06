9e33ade7fabdae1ca7b9af54a4a5874660c43a1e
abhigyaailab <abhigyaailab@gmail.com>
2026-06-06 18:59:11 +0530

    Create bootstrap.py

---
+++ statistics/bootstrap.py
@@ -0,0 +1,21 @@
+from typing import List, Dict, Any
+
+def generate_bootstrap_samples(
+scores: List[float],
+n_iterations: int = 1000
+) -> List[List[float]]:
+"""Generate bootstrap samples."""
+pass
+
+def bootstrap_metric_distribution(
+scores: List[float],
+n_iterations: int = 1000
+) -> List[float]:
+"""Generate bootstrap metric distribution."""
+pass
+
+def compute_bootstrap_mean(
+bootstrap_scores: List[float]
+) -> float:
+"""Compute bootstrap mean."""
+pass
