74f5c2d2ac66ea5a214400ada00d4637e37b3634
abhigyaailab <abhigyaailab@gmail.com>
2026-06-06 18:47:15 +0530

    Create bootstrap_distribution.py

---
+++ results/bootstrap_distribution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def save_bootstrap_distribution(
+distribution: List[float]
+) -> None:
+"""Save bootstrap distribution."""
+pass
+
+def load_bootstrap_distribution() -> List[float]:
+"""Load bootstrap distribution."""
+pass
