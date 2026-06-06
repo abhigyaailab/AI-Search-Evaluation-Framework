d15a608100dd3565e89006856cb7b41e1b0281f5
abhigyaailab <abhigyaailab@gmail.com>
2026-06-06 18:46:42 +0530

    Create query_level_results.py

---
+++ results/query_level_results.py
@@ -0,0 +1,11 @@
+from typing import Dict, List, Any
+
+def save_query_results(
+results: List[Dict[str, Any]]
+) -> None:
+"""Save query-level results."""
+pass
+
+def load_query_results() -> List[Dict[str, Any]]:
+"""Load query-level results."""
+pass
