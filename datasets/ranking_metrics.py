ade2b1d89d8096e8982d38ca95fbc30f7b323f77
abhigyaailab <abhigyaailab@gmail.com>
2026-06-06 18:38:17 +0530

    Create ranking_metrics.py

---
+++ datasets/ranking_metrics.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def compute_ndcg(
+relevance_scores: List[int]
+) -> float:
+"""Compute NDCG."""
+pass
+
+def compute_mrr(
+relevance_scores: List[int]
+) -> float:
+"""Compute Mean Reciprocal Rank."""
+pass
+
+def compute_map(
+relevance_scores: List[int]
+) -> float:
+"""Compute Mean Average Precision."""
+pass
