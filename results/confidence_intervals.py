bde53d817aa903f5b997c330d97dacc3a9dbfb8f
abhigyaailab <abhigyaailab@gmail.com>
2026-06-06 18:47:42 +0530

    Create confidence_intervals.py

---
+++ results/confidence_intervals.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+def save_confidence_interval(
+interval: Dict
+) -> None:
+"""Save confidence interval."""
+pass
+
+def load_confidence_interval() -> Dict:
+"""Load confidence interval."""
+pass
